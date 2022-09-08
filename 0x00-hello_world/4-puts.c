f59a9d6e781b67762578d50cd788800290a32b15
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-08 05:46:38 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+c0;276;0cat#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle"); 
+  return (0); 
+}
