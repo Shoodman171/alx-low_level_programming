b989e91d34636764bdc0206834a74843bf1c342a
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-12 04:39:50 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,25 @@
-#include <stdio.h>                                                                                                                                                                    
+#include <stdio.h>                 
 
-                                                                                                                                                                                      
+/**                                                           
 
-/**                                                                                                                                                                                   
+* main - print the string in the main function                                                    
 
-* main - print the string in the main function                                                                                                                                        
+*                                                                                         
 
-*                                                                                                                                                                                     
+*Description: using the main function                                              
 
-*Description: using the main function                                                                                                                                                 
+*this program prints "Programming is like building a multilingual puzzle
 
-*this program prints "Programming is like building a multilingual puzzle                                                                                                              
+*Return: 0                                                                                       
 
-*Return: 0                                                                                                                                                                            
+*/                                                                                          
 
-*/                                                                                                                                                                                    
+int main(void)                                                                                  
 
-int main(void)                                                                                                                                                                        
+{                                                                                                  
 
-{                                                                                                                                                                                     
+        puts("\"Programming is like building a multilingual puzzle");           
 
-        puts("\"Programming is like building a multilingual puzzle");                                                                                                                 
-
-        return (0);                                                                                                                                                                   
+        return (0);                                                                    
 
 }
