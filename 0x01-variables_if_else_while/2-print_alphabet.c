8490321ab9c90cff28bca2e2247b4c2c8a17f065
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-12 15:51:46 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; lo <= '2'; low++)
+	{
+		putchar(low);
+	}
+		putchar('\n');
+
+	return (0);
+}
