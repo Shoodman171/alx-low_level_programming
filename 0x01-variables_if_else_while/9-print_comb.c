851afbaf6c80ff535cf72cb744494a7edb7a2d83
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-12 20:18:55 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,14 +3,14 @@
  * main - prints all possible combinations of single digits
  *
  * Return: 0 on Success
- */   
+ */ 
 int main(void)
 {
 	int i = '0';
 
 	while (i <= '9')
 	{
-		putchar(1);
+		putchar(i);
 		if (i != '9')
 		{
 			putchar(',');
