5e507708fb9760bca280ec15d2445b6ea7f3c307
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-12 17:01:11 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#incude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low = 'a';
+
+	while (low <= 'z')
+	{
+		if (low != 'q' && low != 'e')
+		{
+			putchar(low);
+		}
+		low++;
+	}
+	putchar('\n');
+	return (0)
+}
