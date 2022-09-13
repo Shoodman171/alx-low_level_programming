5fee48476d02ea5506f5282ba9ac85f1f7b9cd3e
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-12 17:56:42 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -18,7 +18,7 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		print("%d is negative\n", n);
+		printf("%d is negative\n", n);
 
 	return (0);
 }
