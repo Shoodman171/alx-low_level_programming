209b2d3f42268aa77fc493ffa640e56188099dd3
Eniola-rihanat <rihanatlawal@gmail.com>
2022-09-16 06:48:14 -0700

    task main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,10 +7,7 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-<<<<<<< HEAD
 int convert_days(int month, int day);
-=======
 int convert_day(int month, int day);
->>>>>>> 32f71784d7468b5efd1da82ca04d935bf6422545
 
 #endif /* MAIN_H */
