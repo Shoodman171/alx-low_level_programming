d41813d92008e4cdf194e4d3d1cdba50bbeb5fb1
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-09-19 10:38:29 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,9 @@
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of.
- *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+int _strlen(const char *s)
 {
 	size_t length = 0;
 
