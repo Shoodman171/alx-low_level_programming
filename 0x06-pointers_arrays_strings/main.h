a6ad1094c5dff904939513390aaffa0248a6d9d2
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-09-21 19:06:14 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File:main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- * used in the 0x06-pointers_arrays_strings directory.
- */
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
