d6f40449328722db18594184118f44ce45978fdc
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-09-22 21:57:19 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings
- * @dest: Destination string
- * @src: Source string
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
  *
- * Return: A pointer to the resulting string dest
+ * Return: If str1 < str2, the negative difference
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference
  */
-
-char *_strcat(char *dest, char *src)
-
+int _strcmp(char *s1, char *s2)
 {
-
-	int i;
-
-	int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{}
-
-	for (j = 0; src[j]; j++, i++)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-
-		dest[i] = src[j];
+		s1++;
+		s2++;
 	}
 
-	dest[i] = '\0';
-
-return (dest);
+	return (*s1 - *s2);
 }
