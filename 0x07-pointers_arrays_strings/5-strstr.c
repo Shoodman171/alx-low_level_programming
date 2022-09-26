54a5e3312d3b65e6902c163eda42bda2fe0408b0
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-09-26 18:53:34 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 	char *startn = needle, *starth = haystack;
+
 	while (*haystack)
 	{
 		starth = haystack;
 		needle = startn;
+
 		while (*haystack == *needle)
 		{
 			haystack++;
